727b1466bf4904a817e6acc8d07fb1849df17567
kkneville <mskatherinekrueger@gmail.com>
2017-12-06 08:45:26 -0600

    some updates to front end

--- beltclient/src/app/main/list/list.component.ts
+++ beltclient/src/app/main/list/list.component.ts
@@ -17,6 +17,10 @@ export class ListComponent implements OnInit {
   bid = new Bid()
 	users
 	currentUser = new User
+  group = []
+  groups = []
+  list = []
+  matrix = []
 
   constructor(
   	private _MainService: MainService,
@@ -25,11 +29,40 @@ export class ListComponent implements OnInit {
 
   
   ngOnInit() {
+    this.group = []
+    this.groups = []
+    this.matrix = []
+    this.list = []
   	this._MainService.getItems(
       (items) => {
         if (items) {
           this.items = items
           console.log(items)
+          for (var i=0; i<items.length; i++){
+            if (this.group.length == 3){
+              console.log("pushing group into groups")
+              this.groups.push(this.group)
+              this.group = []
+              this.matrix.push(this.list)
+              this.list = []
+              console.log("pushing ", i, " into group" )
+              this.group.push(items[i])
+              this.list.push(true)
+            }
+            else {
+              console.log("pushing ", i, " into group" )
+              this.group.push(items[i])
+              this.list.push(true)
+            }
+          }
+          console.log(this.group.length)
+          if (this.group.length > 0){
+            this.groups.push(this.group)
+            this.group = []
+            this.matrix.push(this.list)
+          }
+          console.log(this.groups)
+          console.log(this.matrix)
         }
         else {
           console.log("no items")
@@ -72,6 +105,37 @@ onSubmit(event){
     }
   )
 }
+
+
+activate(){
+  this.bid = new Bid
+
+}
+
+onBid(event){
+  console.log(this.bid)
+  this._MainService.bid(this.bid,
+    (response) => {
+      if (response) {
+          console.log(response)
+          if (response.reject) {
+            this.alert = response.reject
+            this.bid = new Bid()
+            console.log(this.bid)
+            this.ngOnInit()
+            return
+          }
+          this.alert = ""
+          this.ngOnInit()
+        }
+      else {
+          console.log("no response")
+      }  
+    }
+  )
+}
+
+
 
 updateStatus(event){
   console.log("ending bidding")
